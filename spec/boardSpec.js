c5d767693eb3b8031e4f2f3c3078a979cd337128
Edward Byne <ejbyne@gmail.com>
2015-03-23 18:04:24 +0000

    adds failing unit test for finding other relevant blocks which affect the solutions to a particular block

--- spec/boardSpec.js
+++ spec/boardSpec.js
@@ -64,6 +64,15 @@ describe('Board', function() {
 			expect(board.findBlock('A1')).toEqual(['A1', 'A2', 'A3', 'B1', 'B2', 'B3', 'C1', 'C2', 'C3']);
 		});
 
+		it('knows the other four blocks which affect the solutions to a block', function() {
+			expect(board.findOtherRelevantBlocks('A1')).toEqual(
+				['A4', 'A5', 'A6', 'B4', 'B5', 'B6', 'C4', 'C5', 'C6'],
+				['A7', 'A8', 'A9', 'B7', 'B8', 'B9', 'C7', 'C8', 'C9'],
+				['D1', 'D2', 'D3', 'E1', 'E2', 'E3', 'F1', 'F2', 'F3'],
+				['G1', 'G2', 'G3', 'H1', 'H2', 'H3', 'I1', 'I2', 'I3']
+			)
+		});
+
 	});
 
 });
